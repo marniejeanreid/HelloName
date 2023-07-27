452de030bec2d9f1320a73954e9446b0e57cadfa
Marnie <marnie@kodwithklossy.com>
2023-07-27 11:16:03 +0100

    Initial Commit

---
+++ HelloName/HelloNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloNameApp.swift
+//  HelloName
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
