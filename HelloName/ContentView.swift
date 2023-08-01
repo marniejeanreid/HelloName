df74adbee1aa6c683400566d6f9991e028fec2e2
Marnie <marnie@kodwithklossy.com>
2023-08-01 09:29:47 +0100

    Added comment

--- HelloName/ContentView.swift
+++ HelloName/ContentView.swift
@@ -6,14 +6,31 @@
 //
 
 import SwiftUI
+//COMMENT
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    
+    @State private var textTitle = "What is your name?"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(spacing: 100.0) {
+            Text(textTitle)
+                .font(.title)
+            
+            TextField("Type with your name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+           
+            Button("Submit Name") {
+                textTitle = "Welcome, \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
